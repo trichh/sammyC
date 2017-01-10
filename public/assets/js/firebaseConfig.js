6d5b99f05d957061da7a98c62e48aa8a9e824d19
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2017-01-10 13:16:55 -0500

    Trying to deploy to heroku 2

--- public/assets/js/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDdG0ShMVC_rn1fydq4ZT0snw21SVSB7j0",
-    authDomain: "sammy-c-5df1a.firebaseapp.com",
-    databaseURL: "https://sammy-c-5df1a.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "706957547340"
-  };
-  firebase.initializeApp(config);
