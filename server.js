117fef2b4b3df5c29ab035ae4ed912bcb45d1915
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2017-01-10 13:37:44 -0500

    Editing server.js and trying to deploy to heroku 7

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ var bodyParser = require('body-parser');
 // Run express
 var app = express();
 
-// Parses incoming requests under req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 // Use everything inside of public folder
 app.use(express.static('public'));
-
-// Use file that sends emails
-app.use('/api', require('./api/routes.js'));
 
 // Send index.html on any route
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
+
+// Parses incoming requests under req.body
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Use file that sends emails
+app.use('/api', require('./api/routes.js'));
 
 // Start server
 var server = app.listen(process.env.PORT || 3000, function(){
