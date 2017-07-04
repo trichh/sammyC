fc77c727b5e5b7635daa2455f61036f3ae46aaba
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2017-07-04 11:31:18 -0400

    Making new updates

--- ng/controllers/HomeCtrl.js
+++ ng/controllers/HomeCtrl.js
@@ -1,11 +1,11 @@
 angular.module('sammyC').controller('HomeCtrl', function($scope, $http) {
   var slideIndex = 1;
-  $scope.shop = false;
+  $scope.videos = false;
   showDivs(slideIndex);
   $scope.less = false;
   $scope.more = false;
-  $scope.single = true;
-  $scope.album = false;
+  $scope.single = false;
+  $scope.album = true;
   $scope.contact = false;
 
   function showDivs(n) {
@@ -29,11 +29,11 @@ angular.module('sammyC').controller('HomeCtrl', function($scope, $http) {
     $scope.album = true;
     $scope.more = true;
     $scope.contact = false;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "#FF3300";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
   $scope.showSingles = function() {
@@ -42,11 +42,11 @@ angular.module('sammyC').controller('HomeCtrl', function($scope, $http) {
     $scope.album = false;
     $scope.more = false;
     $scope.contact = false;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "#FF3300";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
   $scope.showContact = function() {
@@ -55,24 +55,24 @@ angular.module('sammyC').controller('HomeCtrl', function($scope, $http) {
     $scope.album = false;
     $scope.more = true;
     $scope.contact = true;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "#FF3300";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
-  $scope.showShop = function() {
+  $scope.showVideos = function() {
     $scope.less = false;
     $scope.single = false;
     $scope.album = false;
     $scope.more = true;
     $scope.contact = false;
-    $scope.shop = true;
+    $scope.videos = true;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "#FF3300";
+    document.getElementById('videos').style.color = "#FF3300";
   }
 
   $scope.showMore = function() {
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,12 +14,12 @@ angular.module('sammyC').config(["$routeProvider", "$locationProvider", function
 
 angular.module('sammyC').controller('HomeCtrl', ["$scope", "$http", function($scope, $http) {
   var slideIndex = 1;
-  $scope.shop = false;
+  $scope.videos = false;
   showDivs(slideIndex);
   $scope.less = false;
   $scope.more = false;
-  $scope.single = true;
-  $scope.album = false;
+  $scope.single = false;
+  $scope.album = true;
   $scope.contact = false;
 
   function showDivs(n) {
@@ -43,11 +43,11 @@ angular.module('sammyC').controller('HomeCtrl', ["$scope", "$http", function($sc
     $scope.album = true;
     $scope.more = true;
     $scope.contact = false;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "#FF3300";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
   $scope.showSingles = function() {
@@ -56,11 +56,11 @@ angular.module('sammyC').controller('HomeCtrl', ["$scope", "$http", function($sc
     $scope.album = false;
     $scope.more = false;
     $scope.contact = false;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "#FF3300";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
   $scope.showContact = function() {
@@ -69,24 +69,24 @@ angular.module('sammyC').controller('HomeCtrl', ["$scope", "$http", function($sc
     $scope.album = false;
     $scope.more = true;
     $scope.contact = true;
-    $scope.shop = false;
+    $scope.videos = false;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "#FF3300";
-    document.getElementById('shop').style.color = "white";
+    document.getElementById('videos').style.color = "white";
   }
 
-  $scope.showShop = function() {
+  $scope.showVideos = function() {
     $scope.less = false;
     $scope.single = false;
     $scope.album = false;
     $scope.more = true;
     $scope.contact = false;
-    $scope.shop = true;
+    $scope.videos = true;
     document.getElementById('albums').style.color = "white";
     document.getElementById('singles').style.color = "white";
     document.getElementById('contact').style.color = "white";
-    document.getElementById('shop').style.color = "#FF3300";
+    document.getElementById('videos').style.color = "#FF3300";
   }
 
   $scope.showMore = function() {
